f36a93be8aba92691167790dc5a27c186f31dd7e
local.hast <local.hast@kakaostyle.com>
2024-12-19 00:01:13 +0900

    feat: Deploy TEST 1

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! - Ver 2"', () => {
+      expect(appController.getHello()).toBe('Hello World! - Ver 2');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! - 20241218 - v1';
+    return 'Hello World! - Ver 2';
   }
 }
